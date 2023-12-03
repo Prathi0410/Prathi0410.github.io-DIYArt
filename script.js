dcd380ea0909b903b6f0a47f3372424b3ff72d81
Prathi0410 <143756168+Prathi0410@users.noreply.github.com>
2023-12-03 17:52:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function applyFilters() {
-    // Add logic to handle filter application
-    console.log('Filters applied!');
-    // You can use AJAX to fetch and display data based on filters
-}
+// Add JavaScript functionality if needed
+document.getElementById('search-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add search functionality here
+});
